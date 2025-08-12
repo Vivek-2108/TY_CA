c834d527a7133e8a90526c05664bb969f7679291
Vivek-2108 <officialvivek5576@gmail.com>
2025-08-12 23:11:02 +0530

    final kmean

--- Kmean algo/kmean.cpp
+++ Kmean algo/kmean.cpp
@@ -14,25 +14,22 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int K = 2;  // number of clusters
-    int N = 100000; // number of points to generate
+    int K = 2;  
+    int N = 500; 
 
-    // Random number generator
+  
     mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
     uniform_real_distribution<double> dist(0.0, 1000.0);
 
     vector<Point> points;
     points.reserve(N);
 
-    // Generate random points
     for(int i = 0; i < N; i++) {
         points.push_back({dist(rng), dist(rng), 0});
     }
 
-    // Pick first K points as initial centroids
     vector<Point> centroids(points.begin(), points.begin() + K);
 
-    // One iteration of K-means
     for(auto &p : points) {
         double minDist = 1e18;
         int clusterId = 0;
@@ -46,7 +43,6 @@ int main() {
         p.cluster = clusterId;
     }
 
-    // Update centroids
     vector<double> sumX(K, 0), sumY(K, 0);
     vector<int> count(K, 0);
     for(auto &p : points) {
@@ -61,7 +57,6 @@ int main() {
         }
     }
 
-    // Save points to file for gnuplot
     ofstream fout("clusters.csv");
     for(auto &p : points) {
         fout << p.x << " " << p.y << " " << p.cluster << "\n";
